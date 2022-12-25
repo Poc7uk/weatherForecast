3a350e4e06dcf74913f7519dfe734044eebf632d
Poc7uk <poc7uk1@gmail.com>
2022-12-25 12:02:30 +0200

    disclaimer fix

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ const App = (): JSX.Element => {
   return (
     
     <main className="relative flex justify-center items-center bg-gradient-to-br from-orange-400 via-purple-400 to-blue-400  h-full sm:h-screen  w-full">
-      <p className="absolute z-10 top-0 mt-[20px] text-zinc-500 text-xs">API is free and has bugs, don`t blame developer :) </p>
+      <p className="absolute z-10 top-0 mt-[10px] text-zinc-500 text-xs">*API is free and has bugs, don`t blame developer :) </p>
       {
       forecast ? (
 
